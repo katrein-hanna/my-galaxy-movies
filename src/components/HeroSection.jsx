ed29ead10956df6a5eaa5b17b61cc20dc661b146
Katrein Hanna <katrein.hanna.96@gmail.com>
2025-05-19 18:36:43 +0300

    feat: add Hero Section component

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import astronaut from "../assets/images/HeroAstro.png";
 
 export default function HeroSection() {
   return (
-    <div className="flex justify-center items-center mt-20">
-      <h1 className="text-4xl">MY GALAXY MOVIES</h1>
-    </div>
+    <>
+      {/* Twinkling stars background */}
+      <div className="absolute inset-0 bg-[url('/stars.png')] bg-repeat opacity-10 pointer-events-none z-0  animate-twinkle" />
+
+      <section className="container mx-auto relative w-full min-h-[90vh] flex flex-col md:flex-row items-center justify-between px-8 md:px-20 py-10  text-bright overflow-hidden">
+        <div className="z-10 max-w-xl space-y-5">
+          <h1 className="text-4xl md:text-6xl font-bold leading-tight">
+            Explore Your <span className="text-glowing">Movie Universe</span>
+          </h1>
+          <p className="text-lg text-bright/80">
+            Build your own galaxy of favorites, add movies to your wishlist,
+            watched list, and more.
+          </p>
+
+          <div className="flex gap-4">
+            <button className="bg-glowing text-primary font-semibold px-6 py-3 rounded-lg hover:bg-highlights hover:text-bright transition">
+              Browse Movies
+            </button>
+            <button className="border border-glowing text-glowing px-6 py-3 rounded-lg hover:bg-glowing hover:text-primary transition">
+              Login to Start
+            </button>
+          </div>
+        </div>
+
+        <div className="z-10 mt-10 md:mt-0">
+          <img
+            src={astronaut}
+            alt="Floating astronaut"
+            className="w-[300px] md:w-[600px] animate-float"
+          />
+        </div>
+      </section>
+    </>
   );
 }
